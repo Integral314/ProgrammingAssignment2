89d8420ea03986bfefb801e66f5fb5872caff9b1
Integral314 <treybeeman@gmail.com>
2015-02-22 06:00:11 -0600

    Solve Matrix and Cache it, then recall cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These function invert a matrix and cache the inverse then
+## check the cache for the matrix if called to invert later
 
-## Write a short comment describing this function
+## makeCacheMatrix will invert a matrix and cache the solution
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function()  x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks the matrix for a cached solution and reports it if found
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
