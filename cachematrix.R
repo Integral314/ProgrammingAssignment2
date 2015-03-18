75d1e20c9de5ca3f9c161a6252d8f2debc7f152d
Integral314 <treybeeman@gmail.com>
2015-03-17 19:28:17 -0500

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,17 @@
 ## makeCacheMatrix will invert a matrix and cache the solution
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## set the value of the vector
         inv <- NULL
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
+        ## get the value of the vector
         get <- function()  x
+        ## set the value of the mean
         setinv <- function(solve) inv <<- solve
+        ## get the value of the mean
         getinv <- function() inv
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
